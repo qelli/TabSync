556e01556a1550fe29d8752f80605189eca391b1
qelli <hello@k3lly.dev>
2024-11-24 19:45:40 -0600

    remove unused code

--- src/main/java/dev/qelli/minecraft/tabSync/connection/models/ConnectionModel.java
+++
@@ -1,7 +0,0 @@
-package dev.qelli.minecraft.tabSync.connection.models;
-
-public abstract class ConnectionModel {
-    public abstract boolean isConnected();
-    public abstract void start();
-    public abstract void stop();
-}
